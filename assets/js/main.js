f714a21366687323352b30c5a3b6efac93f8c63c
YukiHirayama <yuki.hira26a1@gmail.com>
2023-03-16 12:31:57 +0900

    history

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(function($) {
+    $('.slider').slick({
+        arrows: true,
+        autoplay: false,
+        dots: false,
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToScroll: 1,
+    });
+});
