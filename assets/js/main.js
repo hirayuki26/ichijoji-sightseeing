721a67acb1002209a86d1a5f1a3c014dd3f9e115
YukiHirayama <yuki.hira26a1@gmail.com>
2023-03-17 00:23:22 +0900

    food

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
 $(function($) {
+    // $('.bg-switcher').bgSwitcher({
+    //     images: ['assets/images/mansyuin1.jpg', 'assets/images/spring_hiei.jpg'],
+    //     interval: 3000,
+    //     loop: true
+    // });
+
     $('.slider').slick({
         arrows: true,
         autoplay: false,
@@ -7,5 +13,25 @@ $(function($) {
         centerMode: true,
         centerPadding: '0px',
         slidesToScroll: 1,
+    });
+
+    $('.slider-card').slick({
+        arrows: true,
+        autoplay: false,
+        dots: false,
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToScroll: 1,
+    });
+
+    $('.open-btn').on('click', function() {
+        if ($('.open-box').is(':hidden')) {
+            $('.open-box').slideDown();
+            document.getElementById('change').src='assets/images/close.png';
+        } else {
+            $('.open-box').slideUp();
+            document.getElementById('change').src='assets/images/open.png';
+        }
     });
 });
