1eff0ad53b197863081a2f8ecbf202840d7e4f7a
YukiHirayama <yuki.hira26a1@gmail.com>
2023-03-20 04:34:42 +0900

    animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,4 +57,59 @@ $(function($) {
             document.getElementById('change').src='assets/images/close.png';
         }
     });
+
+    const waveItems = document.querySelectorAll('.wave');
+    
+    waveItems.forEach(function(item) {
+        item.animate(
+            [
+                {
+                    offset: 0.00,
+                    transform: 'translate(0, 0)'
+                },
+                {
+                    offset: 0.05,
+                    transform: 'translate(-5%, 0)'
+                },
+                {
+                    offset: 0.10,
+                    transform: 'translate(5%, 0)'
+                },
+                {
+                    offset: 0.15,
+                    transform: 'translate(-5%, 0)'
+                },
+                {
+                    offset: 0.20,
+                    transform: 'translate(5%, 0)'
+                },
+                {
+                    offset: 0.25,
+                    transform: 'translate(-5%, 0)'
+                },
+                {
+                    offset: 0.30,
+                    transform: 'translate(0, 0)'
+                },
+                {
+                    offset: 1.00,
+                    transform: 'translate(0, 0)'
+                }
+            ],
+            {
+                duration: 1500,
+                iterations: Infinity
+            }
+        );
+    });
+
+    $('.pin-btn').click(function () {
+        const speed = 600;
+        let href = $(this).attr("href");
+        let target = $(href == "#" || href == "" ? "html" : href);
+        // ヘッダーの高さ分下げる
+        let position = target.offset().top - 50;
+        $("body,html").animate({ scrollTop: position }, speed, "swing");
+        return false;
+    });
 });
